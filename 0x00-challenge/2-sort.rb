988034aa8ba397ddd98664f824bb47dded9c4f38
hajoura89 <hajarkammou@gmail.com>
2023-09-22 02:17:58 -0700

    sort

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -4,11 +4,10 @@
 #  Fix Error by replacing result.insert(i - 1, i_arg) by result.insert(i, i_arg)#
 #  and replacing result[i] < by <= i_arg
 ###
-
 result = []
 ARGV.each do |arg|
     # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
+    next if arg !~ /^-?\d+$/
 
     # convert to integer
     i_arg = arg.to_i
